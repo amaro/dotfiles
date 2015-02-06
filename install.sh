ee8f0fc72a777d0d198e1e1de674ffe16adb17e3
amaro <amaro@users.noreply.github.com>
2015-02-06 12:49:55 -0500

    added gitconfig.

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 echo "removing previous files"
 rm -rf ~/.vimrc
 rm -rf ~/.vim
+rm -rf ~/.gitconfig
 
 echo "creating new files"
 ln -s ~/configs/vimrc ~/.vimrc
 ln -s ~/configs/vim ~/.vim
+ln -s ~/configs/gitconfig ~/.gitconfig
