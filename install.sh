0e6cba1da85671b9a771a8d5652b0fb3b3d74e8e
amaro <amaro@users.noreply.github.com>
2015-02-06 15:42:37 -0500

    added bash_profile. it sources bashrc

--- install.sh
+++ install.sh
@@ -5,9 +5,11 @@ rm -rf ~/.vimrc
 rm -rf ~/.vim
 rm -rf ~/.gitconfig
 rm -rf ~/.bashrc
+rm -rf ~/.bash_profile
 
 echo "creating new files"
 ln -s ~/configs/vimrc ~/.vimrc
 ln -s ~/configs/vim ~/.vim
 ln -s ~/configs/gitconfig ~/.gitconfig
 ln -s ~/configs/bashrc ~/.bashrc
+ln -s ~/configs/bash_profile ~/.bash_profile
